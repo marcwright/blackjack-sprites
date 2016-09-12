f4381aeaeef54f3f439972c8b4e907eeb8c6ac36
Marc Wright <marcwright@Marcs-MacBook-Pro-2.local>
2016-09-11 22:31:32 -0400

    converted game to object literal

--- script.js
+++ script.js
@@ -1,178 +1,105 @@
 window.onload = function(){
-  var playerCardsArray = [];
-  var dealerCardsArray = [];
-  var playerScore = 0;
-  var dealerScore = 0;
-  var deck = [];
-  var suits = ["spades", "hearts", "clubs", "diamonds"];
+  var game = {
+    playerCardsArray: [],
+    dealerCardsArray: [],
+    playerScore: 0,
+    dealerScore: 0,
+    suits: ["spades", "hearts", "clubs", "diamonds"],
+    hitCounter: 1,
+    deck: [],
+    createDeck: function(){
+      for (var i = 0; i < 4; i++){
+      	for (var j = 0; j < 13; j++) {
+      		var newCard = {};
+      		newCard.suit = this.suits[i];
 
+      		if (j === 0) {
+      			newCard.pattern = "ace";
+      			newCard.val = 11;
+      		} else if (j === 10) {
+      			newCard.pattern = "jack";
+      			newCard.val = 10;
+      		} else if (j === 11) {
+      			newCard.pattern = "queen";
+      			newCard.val = 10;
+      		} else if (j === 12) {
+      			newCard.pattern = "king";
+      			newCard.val = 10;
+      		} else {
+      			newCard.pattern = `${j + 1}`;
+      			newCard.val = parseInt(`${j + 1}`);
+      		}
 
-  // var deck = [
-  // 	{suit: "hearts", score: 2, backgroundPosition: "-800px -130px" },
-  // 	{suit: "hearts", score: 3, backgroundPosition: "-894px -130px"},
-  //   {suit: "hearts", score: 4, backgroundPosition: "-988px -130px"},
-  //   {suit: "hearts", score: 5, backgroundPosition: "-1082px -130px"},
-  //   {suit: "hearts", score: 6, backgroundPosition: "-1176px -130px"},
-  //   {suit: "hearts", score: 7, backgroundPosition: "-1270px -130px"},
-  //   {suit: "hearts", score: 8, backgroundPosition: "-1362px -130px"},
-  //   {suit: "hearts", score: 9, backgroundPosition: "-1456px -130px"},
-  //   {suit: "hearts", score: 10, backgroundPosition: "2196px -130px", type: "jack"},
-  //   {suit: "hearts", score: 10, backgroundPosition: "2102px -130px", type: "queen"},
-  //   {suit: "hearts", score: 10, backgroundPosition: "2008px -130px", type: "king"},
-  //   {suit: "hearts", score: 11, backgroundPosition: "-706px -130px", type: "ace"},
-  //   {suit: "spades", score: 2, backgroundPosition: "-800px -5px"},
-  //   {suit: "spades", score: 3, backgroundPosition: "-894px -5px"},
-  //   {suit: "spades", score: 4, backgroundPosition: "-988px -5px"},
-  //   {suit: "spades", score: 5, backgroundPosition: "-1082px -5px"},
-  //   {suit: "spades", score: 6, backgroundPosition: "-1176px -5px"},
-  //   {suit: "spades", score: 7, backgroundPosition: "-1270px -5px"},
-  //   {suit: "spades", score: 8, backgroundPosition: "-1362px -5px"},
-  //   {suit: "spades", score: 9, backgroundPosition: "-1456px -5px"},
-  //   {suit: "spades", score: 10, backgroundPosition: "2196px -5px", type: "jack"},
-  //   {suit: "spades", score: 10, backgroundPosition: "2102px -5px", type: "queen"},
-  //   {suit: "spades", score: 10, backgroundPosition: "2008px -5px", type: "king"},
-  //   {suit: "spades", score: 11, backgroundPosition: "-706px -5px", type: "ace"},
-  //   {suit: "clubs", score: 2, backgroundPosition: "-800px -254px"},
-  //   {suit: "clubs", score: 3, backgroundPosition: "-894px -254px"},
-  //   {suit: "clubs", score: 4, backgroundPosition: "-988px -254px"},
-  //   {suit: "clubs", score: 5, backgroundPosition: "-1082px -254px"},
-  //   {suit: "clubs", score: 6, backgroundPosition: "-1176px -254px"},
-  //   {suit: "clubs", score: 7, backgroundPosition: "-1270px -254px"},
-  //   {suit: "clubs", score: 8, backgroundPosition: "-1362px -254px"},
-  //   {suit: "clubs", score: 8, backgroundPosition: "-1456px -254px"},
-  //   {suit: "clubs", score: 10, backgroundPosition: "2196px -254px", type: "jack"},
-  //   {suit: "clubs", score: 10, backgroundPosition: "2102px -254px", type: "queen"},
-  //   {suit: "clubs", score: 10, backgroundPosition: "2008px -254px", type: "king"},
-  //   {suit: "clubs", score: 11, backgroundPosition: "-706px -254px", type: "ace"},
-  //   {suit: "diamonds", score: 2, backgroundPosition: "-800px -378px"},
-  //   {suit: "diamonds", score: 3, backgroundPosition: "-894px -378px"},
-  //   {suit: "diamonds", score: 4, backgroundPosition: "-988px -378px"},
-  //   {suit: "diamonds", score: 5, backgroundPosition: "-1082px -378px"},
-  //   {suit: "diamonds", score: 6, backgroundPosition: "-1176px -378px"},
-  //   {suit: "diamonds", score: 7, backgroundPosition: "-1270px -378px"},
-  //   {suit: "diamonds", score: 8, backgroundPosition: "-1362px -378px"},
-  //   {suit: "diamonds", score: 9, backgroundPosition: "-1456px -378px"},
-  //   {suit: "diamonds", score: 10, backgroundPosition: "2196px -378px", type: "jack"},
-  //   {suit: "diamonds", score: 10, backgroundPosition: "2102px -378px", type: "queen"},
-  //   {suit: "diamonds", score: 10, backgroundPosition: "2008px -378px", type: "king"},
-  //   {suit: "diamonds", score: 11, backgroundPosition: "-706px -378px", type: "ace"},
-  // ];
+      		newCard.backgroundPosition = `${-706 + (-93.8 * j)}px ${-5 + (-124 * i)}px`;
 
-var createDeck = function(){
-  for (var i = 0; i < 4; i++){
-  	for (var j = 0; j < 13; j++) {
-  		var newCard = {};
-
-  		newCard.suit = suits[i];
-
-  		if (j === 0) {
-  			newCard.pattern = "ace";
-  			newCard.val = 11;
-  		} else if (j === 10) {
-  			newCard.pattern = "jack";
-  			newCard.val = 10;
-  		} else if (j === 11) {
-  			newCard.pattern = "queen";
-  			newCard.val = 10;
-  		} else if (j === 12) {
-  			newCard.pattern = "king";
-  			newCard.val = 10;
-  		} else {
-  			newCard.pattern = `${j + 1}`;
-  			newCard.val = parseInt(`${j + 1}`);
-  		}
-
-  		newCard.backgroundPosition = `${-706 + (-93.8 * j)}px ${-5 + (-124 * i)}px`;
-
-  		deck.push(newCard);
-  	}
-  }
-};
-
-  console.log(deck);
-
-  var dealCard = function(){
-    var index = Math.floor(Math.random() * deck.length);
-    var card = deck.splice(index, 1)[0];
-    console.log(card);
-    return card;
-  };
-
-  var dealFirstCards = function(){
-  	for (var i = 1; i < 5; i++){
-      var card = dealCard();
-      if (i % 2 !== 0) {
-    		playerCardsArray.push(card);
-        playerScore += card.val;
-      } else {
-        dealerCardsArray.push(card);
-        dealerScore += card.val;
+      		this.deck.push(newCard);
+      	}
       }
-      // (i % 2 !== 0) ? playerCardsArray.push(card) : dealerCardsArray.push(card);
-      // (i % 2 !== 0) ? playerScore += card.score : dealerScore += card.score;
+    },
+    dealCard: function(){
+      var index = Math.floor(Math.random() * this.deck.length);
+      var card = this.deck.splice(index, 1)[0];
+      console.log(card);
+      return card;
+    },
+    dealFirstCards: function(){
+    	for (var i = 1; i < 5; i++){
+        var card = this.dealCard();
+        if (i % 2 !== 0) {
+      		this.playerCardsArray.push(card);
+          this.playerScore += card.val;
+        } else {
+          this.dealerCardsArray.push(card);
+          this.dealerScore += card.val;
+        }
+      }
+    	return [this.playerCardsArray, this.dealerCardsArray];
+    },
+    addFirstCardsToDom: function(){
+      this.playerCardsArray.forEach(function(card, index){
+        var domCard = document.querySelector(`.card${index + 1}`);
+        domCard.style.backgroundPosition = card.backgroundPosition;
+      });
+
+      this.dealerCardsArray.forEach(function(card, index){
+        var domCard = document.querySelector(`.card${index + 3}`);
+        domCard.style.backgroundPosition = card.backgroundPosition;
+      });
+    },
+    updateDomScore: function(){
+      var $dealerScoreBox = $(".dealerScoreBox");
+      var $playerScoreBox = $(".playerScoreBox");
+
+      $dealerScoreBox.html(`DEALER: ${this.dealerScore}`);
+      $playerScoreBox.html(`PLAYER: ${this.playerScore}`);
+    },
+    hitButton: function(){
+      $("button[name='hit']").on('click', function(){
+        var hitCard = game.dealCard();
+
+        game.playerCardsArray.push(hitCard);
+        game.playerScore += hitCard.val;
+
+        var hitCardLeft = `${425 + (25 * game.hitCounter)}px`
+        var hitCardTop = `${375 + (25 * game.hitCounter)}px`
+
+        var playerHitCardElement = `<div class='card${game.hitCounter + 4}' style="background-position: ${hitCard.backgroundPosition}; top:${hitCardTop}; left:${hitCardLeft} "></div>`;
+
+        $('body').append(playerHitCardElement);
+        game.hitCounter += 1;
+        game.updateDomScore();
+        return playerHitCardElement;
+      })
+    },
+    initialize: function(){
+      game.hitButton();
+      game.createDeck();
+      game.dealFirstCards();
+      game.addFirstCardsToDom();
+      game.updateDomScore();
+      console.log(`player: ${game.playerScore} dealer: ${game.dealerScore}`);
     }
-  	return [playerCardsArray, dealerCardsArray];
   };
 
-  var addFirstCardsToDom = function(){
-    // var cardArrays = [playerCardsArray, dealerCardsArray];
-
-    // for (var i = 0; i < 4; i++){
-    //   var domCard = document.querySelector(`.card${index + 1}`);
-    //
-    //   if (i % 2 == 0) {
-    //     domCard.style.backgroundPosition = card.backgroundPosition;
-    //
-    //   }
-    //
-    // }
-
-    playerCardsArray.forEach(function(card, index){
-      var domCard = document.querySelector(`.card${index + 1}`);
-      // domCard.style.display = "block";
-      // domCard.style.animationDelay = `${index}s`;
-      domCard.style.backgroundPosition = card.backgroundPosition;
-    });
-
-    dealerCardsArray.forEach(function(card, index){
-      var domCard = document.querySelector(`.card${index + 3}`);
-      // domCard.style.animationDelay = `${index}s`;
-      domCard.style.backgroundPosition = card.backgroundPosition;
-    });
-  };
-
-  var updateDomScore = function(){
-    var dealerScoreBox = document.querySelector(".dealerScoreBox");
-    var playerScoreBox = document.querySelector(".playerScoreBox");
-    dealerScoreBox.innerHTML = `DEALER: ${dealerScore}`;
-
-    playerScoreBox.innerHTML = `PLAYER: ${playerScore}`
-
-  };
-
-    var hitCounter = 1;
-
-    $("button[name='hit']").on('click', function(){
-      var hitCard = dealCard();
-
-      playerCardsArray.push(hitCard);
-      playerScore += hitCard.val;
-
-      var hitCardLeft = `${425 + (25 * hitCounter)}px`
-      var hitCardTop = `${375 + (25 * hitCounter)}px`
-
-      var playerHitCardElement = `<div class='card${hitCounter}' style="background-position: ${hitCard.backgroundPosition}; top:${hitCardTop}; left:${hitCardLeft} "></div>`;
-
-      $('body').append(playerHitCardElement);
-      hitCounter += 1;
-      updateDomScore();
-    });
-
-
-  createDeck();
-  dealFirstCards();
-  addFirstCardsToDom();
-  updateDomScore();
-  console.log(`player: ${playerScore} dealer: ${dealerScore}`);
-
+  game.initialize();
 }; // end of window.onload()
